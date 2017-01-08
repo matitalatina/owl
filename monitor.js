b626e18f2735bbd944af845bf3e3132ef26e8813
Armando <arpesenti@gmail.com>
2017-01-08 15:19:45 +0000

    fix

--- monitor.js
+++ monitor.js
@@ -14,12 +14,11 @@ wemo.discover(function(deviceInfo) {
 	plugs[UDN] = client;
 
 	client.setBinaryState(0);
-	status[UDN] = 0
+	status[UDN] = 0;
 
 	client.on('binaryState', function(value) {
-		status[UDN] = value;
-    	console.log(UDN + ': %s', value);
-    });
+    		console.log(UDN + ': %s', value);
+    	});
 });
 
 var OWL = require('./owlintuition.js');
@@ -47,19 +46,64 @@ owl.on('electricity', function( event ) {
 });
 
 function control() {
-	for (var UDN in status) {
-		if ((status[UDN] == 0) && (exporting > plugPower)) {
-			plugs[UDN].setBinaryState(1);
-			status[UDN] = 1;
-			console.log(UDN + ": Accesa");
-			return
-		} else if ((status[UDN] > 0) && (consuming > generating)) {
-			plugs[UDN].setBinaryState(0);
-			status[UDN] = 0;
-			console.log(UDN + ": Spenta");
-			return
-		}
+	console.log('Potenza soglia: ' + plugPower);
+	for (var UDN in plugs) {
+		plugs[UDN].getBinaryState(function (err, response) {
+                        //console.log(UDN + ' ' + response + ' ' + err);
+			if (err) { 
+                        	// remove 
+                                plugs.splice(UDN, 1);
+				status.splice(UDN, 1);
+                        } else {
+				var status = parseInt(response);
+				if ((status == 0) && (exporting > plugPower)) {
+					plugs[UDN].setBinaryState(1);
+					status[UDN] = 1;
+				} else if ((status > 0) && (consuming > generating)) {
+					plugs[UDN].setBinaryState(0);
+					status[UDN] = 0;
+				}
+                	}
+		});
 	}
 }
 
 setInterval(control, 5*1000);
+
+var express = require('express');
+var app = express();
+const util = require('util')
+
+app.get('/', function (req, res) {
+	if ('plugPower' in req.query) {
+		plugPower = parseInt(req.query['plugPower']);
+	}
+	var html = '<html><body>';
+	html += '<h1>Prese</h1>';
+	html += '<ul>';
+	for (var UDN in plugs) {
+		html += '<li>'
+		html += plugs[UDN].device.friendlyName
+		if (status[UDN] == 0) {
+			html += ' spenta';	
+		} else {
+			html += ' accesa';
+		}
+		html += '</li>';
+	}
+	html += '</ul>';
+	html += '<h1>Produzione</h1>';
+	html += '<ul>';
+	html += '<li>Generando: ' + generating + '</li>';
+	html += '<li>Consumando: ' + consuming + '</li>';
+	html += '<li>Esportando: ' + exporting + '</li>';	
+	html += '</ul><br/>';
+	html += '<form>Potenza soglia<br/><input type="text" name="plugPower" value="' + plugPower + '"><br/><input type="submit" value="Submit"></form>'
+	html += '</body></html>';
+	res.send(html);
+})
+
+var port = 8080;
+app.listen(port, function () {
+	console.log('Listening on port ' + port);
+})
